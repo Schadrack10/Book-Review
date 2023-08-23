502c6510edba2e34fe9a63794151d2621c5060c7
schadrack10 <schadrackbotombe@gmail.com>
2023-08-23 03:44:04 +0200

    added asyn methods

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,78 +30,105 @@ public_users.post("/register", (req, res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  return res.status(200).json({ books });
+public_users.get('/', async (req, res) => { //async mehod to get all books
+   console.log("geting all books!!!")
+  try {
+    return res.status(200).json({ books: await fetchAllBooks() });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred while fetching books." });
+  }
 });
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  const { isbn } = req.params;
-  const book = books[isbn]; // Access the book directly using the ISBN as the key
+public_users.get('/isbn/:isbn', async (req, res) => { //converted to asynchronous function and using setTimeout to stimulate
+  try {
+    const { isbn } = req.params;
+    const book = await fetchBookByISBN(isbn);
 
-  if (!book) {
-    return res.status(404).json({ message: "Book not found." });
+    if (!book) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+
+    return res.status(200).json({ book });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred while fetching the book." });
   }
-
-  return res.status(200).json({ book });
 });
+
+
+
+
 
 // Delete the book record based on ISBN
-public_users.delete('/isbn/:isbn', function (req, res) {
-  const { isbn } = req.params;
+public_users.delete('/isbn/:isbn', async (req, res) => { // async method to delete books
+  try {
+    const { isbn } = req.params;
 
-  if (!books[isbn]) {
-    return res.status(404).json({ message: "Book not found." });
+    if (!books[isbn]) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+
+    delete books[isbn]; // Delete the book using the ISBN as the key
+
+    console.log(books);
+
+    return res.status(200).json({ message: "Book deleted successfully." });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred while deleting the book." });
   }
-
-  delete books[isbn]; // Delete the book using the ISBN as the key
-
-  console.log(books)
-
-  return res.status(200).json({ message: "Book deleted successfully." });
 });
+
 
 
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-  const { author } = req.params;
-  const booksByAuthor = [];
+public_users.get('/author/:author', async (req, res) => { //async to get book by author
+  try {
+    const { author } = req.params;
+    const booksByAuthor = [];
 
-  // Iterate through the books object to find books by the specified author
-  for (const isbn in books) {
-    if (books[isbn].author === author) {
-      booksByAuthor.push(books[isbn]);
+    // Iterate through the books object to find books by the specified author
+    for (const isbn in books) {
+      if (books[isbn].author === author) {
+        booksByAuthor.push(books[isbn]);
+      }
     }
-  }
 
-  if (booksByAuthor.length === 0) {
-    return res.status(404).json({ message: "No books found for this author." });
-  }
+    if (booksByAuthor.length === 0) {
+      return res.status(404).json({ message: "No books found for this author." });
+    }
 
-  return res.status(200).json({ books: booksByAuthor });
+    return res.status(200).json({ books: booksByAuthor });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred while fetching books by author." });
+  }
 });
 
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-  const { title } = req.params;
-  const booksWithTitle = [];
+public_users.get('/title/:title', async (req, res) => { //async search by title
+  try {
+    const { title } = req.params;
+    const booksWithTitle = [];
 
-  // Iterate through the books object to find books with matching titles
-  for (const isbn in books) {
-    if (books[isbn].title.toLowerCase().includes(title.toLowerCase())) {
-      booksWithTitle.push(books[isbn]);
+    // Iterate through the books object to find books with matching titles
+    for (const isbn in books) {
+      if (books[isbn].title.toLowerCase().includes(title.toLowerCase())) {
+        booksWithTitle.push(books[isbn]);
+      }
     }
-  }
 
-  if (booksWithTitle.length === 0) {
-    return res.status(404).json({ message: "No books found with this title." });
-  }
+    if (booksWithTitle.length === 0) {
+      return res.status(404).json({ message: "No books found with this title." });
+    }
 
-  return res.status(200).json({ books: booksWithTitle });
+    return res.status(200).json({ books: booksWithTitle });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred while fetching books by title." });
+  }
 });
+
 
 
 
@@ -116,6 +143,35 @@ public_users.get('/review/:isbn', function (req, res) {
 
   return res.status(200).json({ reviews: book.reviews });
 });
+
+
+
+
+
+
+//simulated functions
+
+async function fetchAllBooks() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(booksArr);
+    }, 1000); // Simulate a delay of 1 second
+  });
+}
+
+// Simulated asynchronous function to fetch a book by ISBN
+async function fetchBookByISBN(isbn) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(books[isbn]);
+    }, 1000); // Simulate a delay of 1 second
+  });
+}
+
+
+
+
+
 
 
 
